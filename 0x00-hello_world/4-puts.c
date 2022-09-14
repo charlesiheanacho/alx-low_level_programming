e0d37526f99ec2f1966edebd0c04e3545a4c3281
charlesiheanacho <charlesiheanacho01@gmail.com>
2022-09-14 13:07:41 -0700

    goodd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a text
+ * Return: always 0 (sucess)
+ */
+ int main(void)
+   {
+	   puts("\"Programming is like a multilingual puzzle");
+	   return(0);
+   }
