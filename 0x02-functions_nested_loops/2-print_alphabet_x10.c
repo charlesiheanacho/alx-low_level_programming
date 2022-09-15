ee7c93e6fdc36e2166534377182c9369739bcc77
charlesiheanacho <charlesiheanacho01@gmail.com>
2022-09-15 08:37:12 -0700

    koll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int i = '0';
-	char letter = 'a';
+	chiar letter = 'a';
 	
 	while (i <= '9')
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-		}						
+		}
+		
 		_putchar('\n');
 		i++;
 	}
